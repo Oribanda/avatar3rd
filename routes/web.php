4860e1dde024a7a4f07026016c1c1f7073b38268
oribanda <oribanda2@gmail.com>
2020-09-12 21:07:41 +0900

    4thCommit

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('api/upload', 'UploadController');
+
+
 Route::get('/img', 'ImgController@index');
+
+// Route::get('/image_input', 'ImgController@getImageInput');
+// Route::post('/image_confirm', 'ImgController@postImageConfirm');
+// Route::post('/image_complete', 'ImgController@postImageComplete');
+
+
 
 //下記を追記
 //画像をアップロードするページ
-Route::get('/upload', 'Api/UploadController@input');
+Route::get('/upload', 'UploadController@input');
 //画像を保存したり画像名をDBに格納する部分
-Route::post('/upload', 'Api/UploadController@upload');
+Route::post('/upload', 'UploadController@upload');
 //保存した画像を表示するページ
-Route::get('/output', 'Api/UploadController@output');
+Route::get('/output', 'UploadController@output');
 //上記までを追記
+Route::resource('Api/upload', 'UploadController');
+
